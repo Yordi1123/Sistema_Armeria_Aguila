a8383351d7e94f568c5fc3eaa919e9670e753341
yordilv <yhonatanlv11@gmail.com>
2025-07-19 22:43:45 -0500

    he creado la clase venta

---
+++ src/main/java/com/armeria/sistema/modelo/Venta.java
@@ -0,0 +1,8 @@
+package com.armeria.sistema.modelo;
+
+public class Venta {
+    public static void main (String[] args){
+        System.out.println("Hola equipo, Listos para trabajar?");
+
+    }
+}
